62525bed5ac0ad1932700d2aa7e1f40fa87f45a9
NazimFirdousAli <51528955+NazimFirdousAli@users.noreply.github.com>
2021-02-03 18:24:23 +0500

    CRUD with Prisma and Graphql with server and client
    first we have to make prima setup with prisma.io then set up prisma with servergraphql check the query properly with playground then go to the client and make ui proper with graphql client

--- server/index.js
+++ server/index.js
@@ -1,36 +1,8 @@
 const { ApolloServer, gql, ApolloError } = require('apollo-server');
-let ids = 4;
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient();
 
-const students =
-    [
-        {
-            "id": '1',
-            "name": "Nazim",
-            "email": "abc@gmail.com",
-            "age": 22
-        },
 
-        {
-            "id": '2',
-            "name": "Ali",
-            "email": "def@gmail.com",
-            "age": 22
-        },
-
-        {
-            "id": '3',
-            "name": "Khan",
-            "email": "ghi@gmail.com",
-            "age": 22
-        },
-
-        {
-            "id": '4',
-            "name": "Jan",
-            "email": "jkl@gmail.com",
-            "age": 20
-        }
-    ]
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
@@ -38,52 +10,36 @@ const resolvers = {
     Query: {
         //first student is from query and after the arrow function the student comes from the hardcode data
         // we can write business logic here
-        students: () => students,
+        // students: () => students,
+        students: async() =>{
+            return await prisma.student.findMany();
+        }  
     },
     Mutation: {
-
-        addStudent: (root, args, context, info) => {
-            console.log(args.input)
-            ids = ids + 1
-            students.push(
-                {
-
-                    id: ids,
-                    name: args.input.name,
-                    email: args.input.email,
-                    age: args.input.age
-                }
-            )
-            return {
-                id: ids,
-                name: args.input.name,
-                email: args.input.email,
-                age: args.input.age
-            }
+        addStudent: async (root, data, context, info) => {
+            const isStudentEmail = await prisma.student.findUnique({ where: { email: data.email }});
+            if (isStudentEmail) throw new Error('This email has already been taken by someone else...')
+            const student = await prisma.student.create({
+                data
+            })
+            return student
 
         },
-        updateStudent: (root, { id, input }, context, info) => {
-            // check whether the payload is exists
-            const ind = students.findIndex((st) => st.id == id);
-            if (ind === -1) throw new ApolloError('Student not found...')
-
-            // validate the payload
-
-
-            // mutate the payload
-            students[ind] = {
-                ...students[ind],
-                ...input
+        updateStudent: async (root, { id, ...data }, context, info) => {
+            const isUserDuplicate = await prisma.student.findUnique({ where: { email: data.email } });
+            if (isUserDuplicate && isUserDuplicate.id !== id) {
+                throw new Error('Email has already been taken by someone else Please take another');
             }
-
-            // return the predefined response
-            return students[ind];
+            const student = await prisma.student.update({
+                where:{id},
+                data
+            })
+            return student
         },
-        deleteStudent: (root, args, context, info) => {
-            const value = students.findIndex((st) => st.id == args.id);
-            {
-                students.splice(value, 1);
-            }
+        deleteStudent: async (root, {id}, context, info) => {
+            const student = await prisma.student.delete({
+                where:{id}
+            })
             return "Value Deleted"
 
         }
@@ -100,16 +56,16 @@ const typeDefs = gql`
 
 
   type Student {
-    id: ID
-    name: String
-    email: String
-    age: Int
+    id: Int!
+  name: String!
+  email: String!
+  age: Int!
   }
-  input inputStudent {
-    name: String!
-    email: String!
-    age: Int!
-  }
+#   input inputStudent {
+#     name: String!
+#     email: String!
+#     age: Int!
+#   }
 
   type Query {
     students: [Student]
@@ -117,9 +73,9 @@ const typeDefs = gql`
 
   type Mutation {
     # addStudent is just name
-    addStudent(input: inputStudent!): Student!
-    updateStudent(id: ID!, input: inputStudent!): Student!
-    deleteStudent(id: ID!): String!
+    addStudent(name:String!,email:String!,age:Int!): Student!
+    updateStudent(id: Int! ,name: String!, email: String! ,age: Int! ): Student!
+    deleteStudent(id: Int!): String!
   }
 `;
 
